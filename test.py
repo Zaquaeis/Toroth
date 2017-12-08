f48a92d930c1e31a3493cc9b4859ff14d2d343f6
unknown <williamspencer519@gmail.com>
2017-12-08 02:09:40 -0500

    demo

---
+++ test.py
@@ -0,0 +1,27 @@
+import random
+x = 5
+y = 'poop'
+
+print x
+
+if (x > 3):
+    print y
+
+while (x >0):
+    print x
+    x = x-1
+    
+a = [1,2,'anus']
+for entry in a:
+    print entry
+
+number = random.randint(0,10)
+
+def f(a,b):
+    return a + b
+
+print f(3,4)
+
+a = raw_input('how long is your dick? \n')
+print a
+
